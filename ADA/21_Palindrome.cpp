671d9573febd45213e3d8d780cd04c640c1df33b
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-04-12 17:12:16 +0530

    fix palindrome

--- ADA/21_Palindrome.cpp
+++ ADA/21_Palindrome.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    char str[] = "abcdRACECAR";
+    char str[] = "asdf333";
     int max = 0;
     int tempMax = 0;
 
@@ -10,17 +10,19 @@ int main() {
     while (str[len] != '\0')
         len++;
 
-    for (int i = 0; i < len - 1; i++) {
-        int k = i;
+    for (int i = 0; i < len - 2; i++) {
+        int i2 = i;
         tempMax = 0;
         for (int j = len - 1; j >= i + 1; j--) {
-            if (str[k] == str[j]) {
+            int j2 = j;
+            while (str[i2] == str[j2]) {
                 tempMax++;
-                if (k == j - 1) {
+                if (i2 - j2 <= 2) {
                     if (max < tempMax)
                         max = tempMax;
                 }
-                k++;
+                i2++;
+                j2--;
             }
         }
     }
